88772f60ff6763e843077867c399e2fbf0d69317
MahmoudShalabyy <mahmoudelsayedshalabyy4@gmail.com>
2025-09-21 16:00:37 +0300

    Add migrate route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/migrate', function () {
+    \Artisan::call('migrate', ['--force' => true]);
+    \Artisan::call('storage:link');
+    return 'Migrations run successfully!';
+});
